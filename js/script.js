cea5e3b8db80bae5a4acef415fc0892a3ea8826a
maulana reza <rezastein531@gmail.com>
2024-12-21 17:02:54 +0700

    update terbaru

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   menu.classList.toggle("show");
 });
+
+function showPopup(imageSrc) {
+  const popup = document.getElementById("popup");
+  const popupImage = document.getElementById("popupImage");
+  popupImage.src = imageSrc;
+  popup.style.display = "block";
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
